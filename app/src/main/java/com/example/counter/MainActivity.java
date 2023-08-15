80910ea5103850de395d9aeae698d4ba15b35e1b
showaibbinnasir <showaibnishad@gmail.com>
2023-08-15 03:15:24 -0500

    project completed

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -9,11 +9,43 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView counterDisplay;
+    Button increaseBtn;
+    Button decreaseBtn;
+    Button resetBtn;
+    int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counterDisplay = findViewById(R.id.counterDisplay);
+        increaseBtn = findViewById(R.id.increaseBtn);
+        decreaseBtn = findViewById(R.id.decreaseBtn);
+        resetBtn = findViewById(R.id.resetBtn);
 
+        increaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = count + 1;
+                counterDisplay.setText(""+count);
+            }
+        });
+
+        decreaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count --;
+                counterDisplay.setText(""+count);
+            }
+        });
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = 0;
+                counterDisplay.setText(""+count);
+            }
+        });
 
     }
 }
